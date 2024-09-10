686ec4820fc7af6c30e1dcea32b2426c7e2e77e4
Luan Almeida <luan.almeida@jbtec.com.br>
2024-09-10 13:25:39 -0300

    feat: add github workflow

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const ASSET_STORAGE_URL = import.meta.env.VITE_ASSET_STORAGE_URL;
 export const GENERAL_STORAGE_URL = import.meta.env.VITE_GENERAL_STORAGE_URL;
-export const API_URL = import.meta.env.VITE_API_URL;
-export const SEARCH_URL = import.meta.env.VITE_SEARCH_URL;
+export const API_URL = import.meta.env.VITE_API_BASE_URL;
+export const SEARCH_URL = import.meta.env.VITE_BASE_URL_SEARCH;
