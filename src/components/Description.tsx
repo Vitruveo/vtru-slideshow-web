d1ff284a5ca139dd45125d5304c55a7c22f763e5
Luan Almeida <luan.almeida@jbtec.com.br>
2024-09-12 13:28:17 -0300

    feat: make qr code more performatic and add elipssis overflow in title

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -5,7 +5,9 @@ interface DescriptionProps {
 export function Description({ description }: DescriptionProps) {
   return (
     <div className="flex flex-col gap-2">
-      <h2 className="max-w-full text-center">{description}</h2>
+      <h2 className="max-w-full text-center overflow-hidden text-ellipsis line-clamp-4">
+        {description}
+      </h2>
     </div>
   );
 }
--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -12,7 +12,7 @@ export const QRCode = ({
   value,
   windowOrientation,
 }: QRCodeProps) => {
-  const logoSize = size * 0.25;
+  const logoSize = size * 0.15;
 
   return (
     <div className="flex flex-col gap-4 items-center mx-auto qrCode">
