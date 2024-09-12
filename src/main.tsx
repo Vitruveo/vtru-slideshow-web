788607ca55983acba410bc185cd56b92a0761d74
Luan Almeida <luan.almeida@jbtec.com.br>
2024-09-12 09:49:11 -0300

    feat: add version in meta tag

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { Helmet } from "react-helmet";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import packageInfo from "../package.json";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Helmet>
+      <meta name="version" content={packageInfo.version} />
+    </Helmet>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
